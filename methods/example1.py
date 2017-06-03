73c54a1d2fe5d4fb20f33619411d8c3801debbc6
Juan Carlos <jamserv@gmail.com>
2017-06-02 21:06:18 -0500

    list-pending

--- methods/example1.py
+++ methods/example1.py
@@ -17,4 +17,28 @@ def appendExample():
     
     print fam
     
-appendExample()
+def upperExample():
+    # string to experiment with: room
+    room = "poolhouse"
+
+    # Use upper() on room: room_up
+    room_up = room.upper()
+
+    # Print out room and room_up
+    print(room)
+    print(room_up)
+
+    # Print out the number of o's in room
+    print(room.count("o"))    
+    
+def listMetho():
+    # Create list areas
+    areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+    # Print out the index of the element 20.0
+    print(areas.index('11.25'))
+
+    # Print out how often 14.5 appears in areas
+        
+    
+listMetho()
